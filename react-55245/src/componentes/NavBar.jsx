7a3c5408bd3ce1bd08cc6eabc449ad9afa0ba001
Santiago Torres <santt2498@gmail.com>
2023-07-03 22:12:54 -0300

    Commit agregando NavBar

---
+++ react-55245/src/componentes/NavBar.jsx
@@ -0,0 +1,16 @@
+
+
+export const NavBar = () => {
+
+    return(
+        <div>
+            <ul>
+                <li>Celulares</li>
+                <li>Laptops</li>
+                <li>Componentes</li>
+                <li>Equipos armados</li>
+                <li>Accesorios</li>
+            </ul>
+        </div>
+    )
+}
